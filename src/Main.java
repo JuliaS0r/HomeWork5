7d239828e7a2d286e63e2368e524e3de26bd1ec5
JuliaS0r <Yulyasor0501@yandex.ru>
2023-10-25 05:19:18 +0300

    проверьте дз по условным операторам 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //task1
+        int clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS ==1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+        //task2
+        int clientDeviceYear = 2015;
+        if (clientDeviceYear >= 2015) {
+            if (clientOS == 0) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else if (clientOS == 1) {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        } else {
+            if (clientOS == 0) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else if (clientOS == 1) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        }
+
+        //task3
+        int year = 2021;
+        if (year % 400 == 0) {
+                System.out.println(year + " - високосный год");
+        } else if (year % 4 == 0 && year % 100 != 0) {
+                System.out.println(year + " - високосный год");
+        } else {
+                System.out.println(year + " - не високосный год");
+        }
+
     }
 }
