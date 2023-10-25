4e10d93d1eb6dad80dcd2a38469600a7c7d0b745
JuliaS0r <Yulyasor0501@yandex.ru>
2023-10-25 05:37:14 +0300

    проверьте дз по условным операторам 2

--- src/Main.java
+++ src/Main.java
@@ -34,5 +34,39 @@ public class Main {
                 System.out.println(year + " - не високосный год");
         }
 
+        //task4
+        int deliveryDistance = 95;
+        int deliveryTime = 1;
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется дней: " + deliveryTime);
+        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+            deliveryTime = deliveryTime + 1;
+            System.out.println("Потребуется дней: " + deliveryTime);
+        } else if (deliveryDistance >= 60 && deliveryDistance < 100) {
+            deliveryTime = deliveryTime + 2;
+            System.out.println("Потребуется дней: " + deliveryTime);
+        } else {
+            deliveryTime = deliveryTime + 3;
+            System.out.println("Потребуется дней: " + deliveryTime);
+        }
+
+        //task5
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 1,2,12:
+                System.out.println("Зима");
+                break;
+            case 3,4,5:
+                System.out.println("Весна");
+                break;
+            case 6,7,8:
+                System.out.println("Лето");
+                break;
+            case 9,10,11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Такого месяца нет ");
+        }
     }
 }
